e3d94da57952059077f7b403efc6547d957c1c48
k3zeroua <k3zeroua@enib.fr>
2025-07-27 15:34:21 +0200

    Ajout du dossier beauty_salon sans le dépôt Git interne

--- reactjs_weather_app-master/src/app/store/index.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from "@reduxjs/toolkit";
-import WeatherSlice from "../../features/weather/WeatherSlice";
-
-export const store = configureStore({
-        reducer: {
-            weather: WeatherSlice
-        }
-    }
-)
